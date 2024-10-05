f89de0af3603fd415c93970e89edbe76172cc67b
RomyGolman <golmanromy@gmail.com>
2024-10-05 22:37:27 +0300

    first commit

---
+++ Random Maze Generator/Random Maze Generator/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Random_Maze_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb_width = new System.Windows.Forms.TextBox();
+            this.tb_height = new System.Windows.Forms.TextBox();
+            this.lbl_width = new System.Windows.Forms.Label();
+            this.lbl_height = new System.Windows.Forms.Label();
+            this.btn_genMaze = new System.Windows.Forms.Button();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.btn_i1 = new System.Windows.Forms.Button();
+            this.btn_i2 = new System.Windows.Forms.Button();
+            this.btn_newgen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_width
+            // 
+            this.tb_width.Location = new System.Drawing.Point(76, 5);
+            this.tb_width.Name = "tb_width";
+            this.tb_width.Size = new System.Drawing.Size(132, 20);
+            this.tb_width.TabIndex = 2;
+            this.tb_width.Text = "10";
+            // 
+            // tb_height
+            // 
+            this.tb_height.Location = new System.Drawing.Point(76, 38);
+            this.tb_height.Name = "tb_height";
+            this.tb_height.Size = new System.Drawing.Size(132, 20);
+            this.tb_height.TabIndex = 3;
+            this.tb_height.Text = "10";
+            // 
+            // lbl_width
+            // 
+            this.lbl_width.AutoSize = true;
+            this.lbl_width.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_width.Location = new System.Drawing.Point(12, 38);
+            this.lbl_width.Name = "lbl_width";
+            this.lbl_width.Size = new System.Drawing.Size(58, 16);
+            this.lbl_width.TabIndex = 4;
+            this.lbl_width.Text = "Высота:";
+            // 
+            // lbl_height
+            // 
+            this.lbl_height.AutoSize = true;
+            this.lbl_height.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_height.Location = new System.Drawing.Point(12, 9);
+            this.lbl_height.Name = "lbl_height";
+            this.lbl_height.Size = new System.Drawing.Size(58, 16);
+            this.lbl_height.TabIndex = 5;
+            this.lbl_height.Text = "Ширина";
+            // 
+            // btn_genMaze
+            // 
+            this.btn_genMaze.Location = new System.Drawing.Point(76, 77);
+            this.btn_genMaze.Name = "btn_genMaze";
+            this.btn_genMaze.Size = new System.Drawing.Size(102, 23);
+            this.btn_genMaze.TabIndex = 6;
+            this.btn_genMaze.Text = "Создать";
+            this.btn_genMaze.UseVisualStyleBackColor = true;
+            this.btn_genMaze.Click += new System.EventHandler(this.btn_genMaze_Click);
+            // 
+            // btn_back
+            // 
+            this.btn_back.Enabled = false;
+            this.btn_back.Location = new System.Drawing.Point(9, 57);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(61, 23);
+            this.btn_back.TabIndex = 8;
+            this.btn_back.Text = "Назад";
+            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Visible = false;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // btn_i1
+            // 
+            this.btn_i1.Location = new System.Drawing.Point(214, 38);
+            this.btn_i1.Name = "btn_i1";
+            this.btn_i1.Size = new System.Drawing.Size(15, 28);
+            this.btn_i1.TabIndex = 9;
+            this.btn_i1.Text = "i";
+            this.btn_i1.UseVisualStyleBackColor = true;
+            this.btn_i1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_i2
+            // 
+            this.btn_i2.Location = new System.Drawing.Point(214, -3);
+            this.btn_i2.Name = "btn_i2";
+            this.btn_i2.Size = new System.Drawing.Size(15, 28);
+            this.btn_i2.TabIndex = 10;
+            this.btn_i2.Text = "i";
+            this.btn_i2.UseVisualStyleBackColor = true;
+            this.btn_i2.Click += new System.EventHandler(this.btn_i2_Click);
+            // 
+            // btn_newgen
+            // 
+            this.btn_newgen.Enabled = false;
+            this.btn_newgen.Location = new System.Drawing.Point(9, 77);
+            this.btn_newgen.Name = "btn_newgen";
+            this.btn_newgen.Size = new System.Drawing.Size(61, 23);
+            this.btn_newgen.TabIndex = 11;
+            this.btn_newgen.Text = "Новый";
+            this.btn_newgen.UseVisualStyleBackColor = true;
+            this.btn_newgen.Visible = false;
+            this.btn_newgen.Click += new System.EventHandler(this.btn_newgen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(262, 110);
+            this.Controls.Add(this.btn_newgen);
+            this.Controls.Add(this.btn_i2);
+            this.Controls.Add(this.btn_i1);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.btn_genMaze);
+            this.Controls.Add(this.lbl_height);
+            this.Controls.Add(this.lbl_width);
+            this.Controls.Add(this.tb_height);
+            this.Controls.Add(this.tb_width);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Random Maze Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tb_width;
+        private System.Windows.Forms.TextBox tb_height;
+        private System.Windows.Forms.Label lbl_width;
+        private System.Windows.Forms.Label lbl_height;
+        private System.Windows.Forms.Button btn_genMaze;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Button btn_i1;
+        private System.Windows.Forms.Button btn_i2;
+        private System.Windows.Forms.Button btn_newgen;
+    }
+}
+
---
+++ Random Maze Generator/Random Maze Generator/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Random_Maze_Generator
+{
+    public partial class Form1 : Form
+    {
+        int row, col;
+        bool invalid;
+        int cellWidth, cellCount, dir;
+        Graph[,] theGraph;
+        Stack theStack;
+        Random rand;
+        Graphics g;
+        Pen thePen,whitePen;
+        Brush theBrush,white;
+        int x1, y1, x2, y2;
+        int poppedVal;
+        int flag;
+        int flag2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("(до 50)");
+        }
+
+        private void btn_i2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("(до 80)");
+        }
+
+        private void btn_newgen_Click(object sender, EventArgs e)
+        {
+            flag2++;
+            Controls.Remove(thePanel);
+            btn_genMaze_Click(sender,e);
+        }
+
+        Panel thePanel;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cellWidth = 20;
+            theStack = new Stack();
+            rand = new Random();
+            cellCount = 0;
+            theBrush = new SolidBrush(Color.Black);
+            thePen = new Pen(theBrush);
+            white = new SolidBrush(Color.WhiteSmoke);
+            whitePen = new Pen(white);
+        }
+
+        private void btn_genMaze_Click(object sender, EventArgs e)
+        {
+            invalid = false;
+            flag = 0;
+            if(flag2 != 0)
+            {
+                flag = 0;
+                btn_back.Visible = false;
+                btn_newgen.Visible = false;
+            }
+
+            try
+            {
+                row = Convert.ToInt32(tb_height.Text.Trim());
+                col = Convert.ToInt32(tb_width.Text.Trim());
+                if (row > 50 || col > 80 || row < 1 || col < 1)
+                {
+                    MessageBox.Show("от 1 до 70");
+                    throw new Exception();
+                }
+            }
+            catch (Exception E) { invalid = true; }
+
+            if (tb_width.Text != "" && tb_height.Text != "" && invalid == false)
+            {
+                removeCurrentControls();
+                BackColor = Color.WhiteSmoke;
+                thePanel = new Panel();
+                thePanel.Width = (col + 2) * cellWidth;   //620
+                thePanel.Height = (row + 5) * cellWidth;  //620
+                thePanel.Location = new Point(5, 5);
+                thePanel.Paint += new PaintEventHandler(thePanel_Paint);
+                Controls.Add(thePanel);
+                Width = (col + 2) * cellWidth;
+                Height = (row + 5) * cellWidth;
+                initializeGraph();
+                g = thePanel.CreateGraphics();
+            }
+        }
+
+        private void initializeGraph()
+        {
+            theGraph = new Graph[row, col];
+
+            for (int i = 0; i < theGraph.GetLength(0); i++)
+            {
+                for (int j = 0; j < theGraph.GetLength(1); j++)
+                {
+                    theGraph[i, j] = new Graph();
+                    theGraph[i, j].visited = false;
+                    theGraph[i, j].up = theGraph[i, j].down = theGraph[i, j].left = theGraph[i, j].right = false;
+                    theGraph[i, j].cellNumber = ++cellCount;
+                }
+            }
+        }
+
+        public void thePanel_Paint(object sender, PaintEventArgs e)
+        {
+            x1 = 0;
+            y1 = 0;
+            x2 = cellWidth;
+            y2 = 0;
+
+            for (int i = 0; i < theGraph.GetLength(0) + 1; i++)
+            {
+                for (int j = 0; j < theGraph.GetLength(1); j++)
+                {
+                    if (flag == 0) g.DrawLine(thePen, x1, y1, x2, y2);
+                    x1 = x2;
+                    x2 += cellWidth;
+                }
+                y1 = y2 += cellWidth;
+                x1 = 0;
+                x2 = cellWidth;
+            }
+            g.DrawLine(whitePen, x1, y1, x2, y2);
+            x1 = 0;
+            y1 = 0;
+            x2 = 0;
+            y2 = cellWidth;
+
+            for (int i = 0; i < theGraph.GetLength(0); i++)
+            {
+                for (int j = 0; j < theGraph.GetLength(1) + 1; j++)
+                {
+                    if (flag == 0) g.DrawLine(thePen, x1, y1, x2, y2);
+                    x1 = x2 += cellWidth;
+                }
+                y1 = y2;
+                y2 += cellWidth;
+                x1 = x2 = 0;
+            }
+
+            if (flag == 0) traverseGraph(0, 0, 1);
+            flag++;
+            btn_back.Visible = true;
+            btn_back.Location = new Point(row / 2, Height - 80);
+            btn_newgen.Visible = true;
+            Point u = btn_back.Location;
+            btn_newgen.Location = new Point((col / 2) * cellWidth, Height - 80);
+            btn_back.Enabled = true;
+            btn_newgen.Enabled = true;
+        }
+
+        private void traverseGraph(int r, int c, int cellNumber)
+        {
+            theStack.push(cellNumber);
+            theGraph[r, c].visited = true;
+            g.DrawLine(whitePen, 0, 20, 0, 0);
+            g.DrawLine(whitePen, 20 * col , 20 * row - 20, 20 * col , 20 * row);
+            do
+            {
+                if (theGraph[r, c].up == true && theGraph[r, c].down == true && theGraph[r, c].left == true && theGraph[r, c].right == true)
+                {
+                    poppedVal = theStack.pop();
+                    return;
+                }
+                else
+                {
+                    dir = rand.Next(1, 5);
+                    try
+                    {
+                        if (dir == 1 && theGraph[r, c].up == false)
+                        {
+                            theGraph[r, c].up = true;
+                            if (theGraph[r - 1, c].visited == false)
+                            {
+                                x1 = (c * cellWidth) + 1;
+                                y1 = y2 = (r * cellWidth);
+                                x2 = x1 + (cellWidth - 2);
+                                g.DrawLine(whitePen, x1, y1, x2, y2);
+                                traverseGraph(r - 1, c, theGraph[r - 1, c].cellNumber);
+                            }
+                        }
+                        else if (dir == 2 && theGraph[r, c].right == false)
+                        {
+                            theGraph[r, c].right = true;
+                            if (theGraph[r, c + 1].visited == false)
+                            {
+                                x1 = x2 = (c + 1) * cellWidth;
+                                y1 = (r * cellWidth) + 1;
+                                y2 = y1 + (cellWidth - 2);
+                                g.DrawLine(whitePen, x1, y1, x2, y2);
+                                traverseGraph(r, c + 1, theGraph[r, c + 1].cellNumber);
+                            }
+                        }
+                        else if (dir == 3 && theGraph[r, c].down == false)
+                        {
+                            theGraph[r, c].down = true;
+                            if (theGraph[r + 1, c].visited == false)
+                            {
+                                x1 = (c * cellWidth) + 1;
+                                y1 = y2 = (r + 1) * cellWidth;
+                                x2 = x1 + (cellWidth - 2);
+                                g.DrawLine(whitePen, x1, y1, x2, y2);
+                                traverseGraph(r + 1, c, theGraph[r + 1, c].cellNumber);
+                            }
+                        }
+                        else if (dir == 4 && theGraph[r, c].left == false)
+                        {
+                            theGraph[r, c].left = true;
+                            if (theGraph[r, c - 1].visited == false)
+                            {
+                                x1 = x2 = c * cellWidth;
+                                y1 = (r * cellWidth) + 1;
+                                y2 = y1 + (cellWidth - 2);
+                                g.DrawLine(whitePen, x1, y1, x2, y2);
+                                traverseGraph(r, c - 1, theGraph[r, c - 1].cellNumber);
+                            }
+                        }
+                    }
+                    catch (Exception e) { }
+                }
+            } while (true);
+            
+        }
+
+        private void removeCurrentControls()
+        {
+            lbl_width.Visible = false;
+            lbl_height.Visible = false;
+            tb_width.Visible = false;
+            tb_height.Visible = false;
+            btn_genMaze.Visible = false;
+            btn_i1.Visible = false;
+            btn_i2.Visible = false;
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            Controls.Remove(thePanel);
+            lbl_width.Visible = true;
+            lbl_height.Visible = true;
+            tb_width.Visible = true;
+            tb_height.Visible = true;
+            btn_genMaze.Visible = true;
+            btn_i1.Visible = true;
+            btn_i2.Visible = true;
+            Width = 282; 
+            Height = 153;
+            BackColor = Color.WhiteSmoke;
+            btn_back.Visible = false;
+            btn_back.Enabled = false;
+            btn_newgen.Visible = false;
+            btn_newgen.Enabled = false;
+        }
+    }
+
+    class Graph
+    {
+        public bool visited, up, down, left, right;
+        public int cellNumber;
+    }
+
+    class Stack
+    {
+        public int[] theStack;
+        int top;
+
+        public Stack()
+        {
+            top = 0;
+            theStack = new int[10];
+        }
+
+        public void push(int num)
+        {
+            if (top < theStack.Length) theStack[top++] = num;
+
+            else
+            {
+                int[] temp = new int[top + 10];
+                for (int i = 0; i < theStack.Length; i++) temp[i] = theStack[i];
+
+                theStack = temp;
+                theStack[top++] = num;
+            }
+        }
+
+        public int pop()
+        {
+            if (top > 0) return theStack[--top];
+
+            else return -1;
+        }
+    }
+}
